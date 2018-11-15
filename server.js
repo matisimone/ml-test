e5f6b9909308ea871b4bc0ac349cbb762217ee86
GLOBANT\matias.simone <matias.simone@globant.com>
2018-11-14 23:50:32 -0300

    fixed minor issues

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require('express');
-
 const items = require('./routes/api/items')
-
 const app = express();
 
-app.use(express.urlencoded());
-
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', (req, res) => {res.send('Server running...')});
 
 //routes
 app.use('/api/items', items);
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server running on port ${port}`));
